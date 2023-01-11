4f7b65f04bc024a4285d92dd5d7ab39c6498e42d
Jannic <99671946+jnnic@users.noreply.github.com>
2023-01-11 11:44:47 +0100

    Add files via upload

---
+++ telebot2.py
@@ -0,0 +1,26 @@
+import random
+from telegram.ext import Updater, CommandHandler
+
+PHOTO_PATH = 'example.png'
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="This Bot selects a random line that you send. For example, you can send names. Use this format:")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="/random\nname_1\nname_2\nname_3\nname_4\nname_5")
+    #context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(PHOTO_PATH, 'rb'))
+    
+
+def random_name(update, context):
+    names = update.message.text.split('\n')
+    chosen_name = random.choice(names)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"The chosen name is: {chosen_name}")
+
+def pic(update, context):
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(PHOTO_PATH, 'rb'))
+
+updater = Updater(token='5845164266:AAHGsthZqqDPydKZIKXDKcn4KAn4medusIc', use_context=True)
+
+updater.dispatcher.add_handler(CommandHandler('start', start)) 
+updater.dispatcher.add_handler(CommandHandler('random', random_name))
+updater.dispatcher.add_handler(CommandHandler('pic', pic))
+
+updater.start_polling()
